fa0464fb9949bd83f0c8287d649e93779614a146
Sai Kumar Reddy Baddala <saibaddala172@gmail.com>
2024-08-12 21:04:33 +0530

    Done

---
+++ keys.dart
@@ -0,0 +1,4 @@
+const openAIAPIKey =
+    'sk-H4emchynm8TTLwXbcduTC-7dmuJYFRAADrQu9BNVMtT3BlbkFJLgeg2SYaKH32Nc7cVBPVQypIUZZgl7nkxeaTvtc-8A';
+const anotherAPIKey =
+    'sk-tLDqMc4oyRixV_9YC3cg6XCNCaXR4niMbFtDi7v6uDT3BlbkFJrEQPJfskqICbfi7Fith8U7MMuQcu9E5eJPa9zF5hkA';
